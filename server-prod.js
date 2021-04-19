72e002529bef8a3066bd835a60c2acd925c31bdb
YIRer <xeroonelove@gmail.com>
2021-04-20 01:57:14 +0900

    deploy 명령어 추가

---
+++ server-prod.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const jsonServer = require("json-server");
+const path = require("path");
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema.js");
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.use("/api", jsonServer.router("./db/db.json"));
+app.use("/graphql", expressGraphQL.graphqlHTTP({ graphiql: true, schema }));
+
+app.use("/", express.static(`${__dirname}/build`));
+app.get("*", (_req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
