8169aec97826503281b362fc92190c61de0fb7a6
YIRer <xeroonelove@gmail.com>
2021-04-20 01:12:14 +0900

    오타 수정

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const hostname =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8080/"
-    : "http://umamusume-trainers.me/";
+    ? "http://localhost:8080"
+    : "http://umamusume-trainers.me";
 
 const dbServer = `${hostname}/api`;
 const graphqlServer = `${hostname}/graphql`;
