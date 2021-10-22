c61055347ab76e808b8410abdac80a5f71a039d3
YIRer <xeroonelove@gmail.com>
2021-10-22 19:37:16 +0900

    refact: 검색 필터 적용을 위한 리듀서 사용 및 분기 추가

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,45 +1,88 @@
-import React, { useState } from "react";
+import React, { Dispatch, useReducer } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { UmamusumeType } from "types/Umamusume/umamusume";
 import { SkillType } from "types/Skill/skill";
+import { CardType } from "types/Card/card";
+import {
+  searchUmamusumeHelper,
+  searchCardsHelper,
+  searchSkillsHelper,
+} from "helper";
+
+import { initialState, reducer, ACTION_TYPES } from "./SearchReducers";
 
 const useStyles = makeStyles((_theme) => ({
   wrapper: {
     display: "flex",
     marginBottom: "36px",
+    flexDirection: "column",
   },
+
+  searchWrapper: {
+    display: "flex",
+  },
+
   input: {
     width: "100%",
   },
   searchBtn: {
     marginLeft: "16px",
   },
+  filterWrapper: {},
 }));
 
 type SearchFormProps = {
-  data: UmamusumeType | SkillType;
+  data: Array<UmamusumeType | SkillType | CardType>;
+  handleSearch: Dispatch<Array<UmamusumeType | SkillType | CardType>>;
+  searchType: "Umamusume" | "Card" | "Skill";
 };
 
-export default function SearchForm({ data, handleSearch }) {
+export default function SearchForm({
+  data,
+  handleSearch,
+  searchType,
+}: SearchFormProps) {
   const classes = useStyles();
-  const [keyword, setKeyword] = useState("");
+  const [searchOptions, stateDispatch] = useReducer(reducer, initialState);
 
-  const handleChange = (
+  const handleChangeKeyword = (
     e: React.ChangeEvent<HTMLInputElement> & React.FocusEvent<HTMLInputElement>
   ) => {
     const value = e.target.value;
-    setKeyword(value);
+    stateDispatch({
+      type: ACTION_TYPES.UPDATE_KEYWORD_FILTER,
+      payload: value,
+    });
   };
 
   const onSearch = () => {
-    const searchData = keyword
-      ? data.filter(({ name }) => {
-          return name?.ko?.includes(keyword) || name?.ja?.includes(keyword);
-        })
-      : data;
+    let searchData = [].concat(data);
+    switch (searchType) {
+      case "Umamusume":
+        searchData = searchUmamusumeHelper({
+          data: data as UmamusumeType[],
+          searchOptions,
+        });
+        break;
+      case "Card":
+        searchData = searchCardsHelper({
+          data: data as CardType[],
+          searchOptions,
+        });
+        break;
+      case "Skill":
+        searchData = searchSkillsHelper({
+          data: data as SkillType[],
+          searchOptions,
+        });
+        break;
+      default:
+        break;
+    }
+
     handleSearch(searchData);
   };
 
@@ -51,22 +94,24 @@ export default function SearchForm({ data, handleSearch }) {
 
   return (
     <div className={classes.wrapper}>
-      <TextField
-        classes={{ root: classes.input }}
-        onChange={handleChange}
-        onBlur={handleChange}
-        label="이름 입력"
-        id="search"
-        onKeyUp={handleKeyUp}
-      />
-      <Button
-        className={classes.searchBtn}
-        variant="contained"
-        color="primary"
-        onClick={onSearch}
-      >
-        검색
-      </Button>
+      <div className={classes.searchWrapper}>
+        <TextField
+          classes={{ root: classes.input }}
+          onChange={handleChangeKeyword}
+          onBlur={handleChangeKeyword}
+          label="이름 입력"
+          id="search"
+          onKeyUp={handleKeyUp}
+        />
+        <Button
+          className={classes.searchBtn}
+          variant="contained"
+          color="primary"
+          onClick={onSearch}
+        >
+          검색
+        </Button>
+      </div>
     </div>
   );
 }
