85fa9c9a3ba5a0fa221d7ee10faa4fa479f2ec6d
YIRer <xeroonelove@gmail.com>
2021-11-15 20:48:44 +0900

    refact: apollo-client 설정 변경

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -7,6 +7,27 @@ export default withApollo(({ initialState }) => {
   return new ApolloClient({
     cache: new InMemoryCache({
       addTypename: false,
+      typePolicies: {
+        Query: {
+          fields: {
+            cards: {
+              merge(_existing, incoming) {
+                return incoming;
+              },
+            },
+            umamusumeList: {
+              merge(_existing, incoming) {
+                return incoming;
+              },
+            },
+            skills: {
+              merge(_existing, incoming) {
+                return incoming;
+              },
+            },
+          },
+        },
+      },
     }).restore(initialState || {}),
 
     uri: graphqlServer,
