4c4f4ef50a8e689f78c81b419cdec3fb3e47a017
YIRer <xeroonelove@gmail.com>
2021-11-15 20:48:28 +0900

    refact: node  server를 사용하도록 변경

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express = require("express");
+const next = require("next");
 const jsonServer = require("json-server");
-const path = require("path");
-const cors = require("cors");
 const expressGraphQL = require("express-graphql");
 const schema = require("./schema/schema.js");
-const app = express();
 
-const port = process.env.PORT || 8080;
+const isDev = process.env.NODE_ENV === "development";
 
-if (process.env.NODE_ENV === "development") {
-  app.use(cors());
-}
+const app = next({ dev: isDev });
 
-app.use("/api", jsonServer.router("./db/db.json"));
-app.use("/graphql", expressGraphQL.graphqlHTTP({ graphiql: true, schema }));
+const handle = app.getRequestHandler();
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(`${__dirname}/build/images`, { maxAge: 86400 }));
-  app.use("/", express.static(`${__dirname}/build`));
-  app.get("*", (_req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+app.prepare().then(() => {
+  const server = express();
+
+  const port = process.env.PORT || 8080;
+
+  server.use("/api", jsonServer.router("./db/db.json"));
+  server.use(
+    "/graphql",
+    expressGraphQL.graphqlHTTP({ graphiql: isDev, schema })
+  );
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
   });
-}
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  server.listen(port, () => console.log(`Listening on port ${port}`));
+});
