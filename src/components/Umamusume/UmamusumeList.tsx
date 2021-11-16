82479ba3872596645c0358e329a53b14b9c18443
YIRer <xeroonelove@gmail.com>
2021-11-16 18:02:04 +0900

    refact: pages에서 데이터를 받아서 사용하도록 변경

---
+++ src/components/Umamusume/UmamusumeList.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+import Error from "next/error";
+
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import AddCircleOutlineRoundedIcon from "@material-ui/icons/AddCircleOutlineRounded";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+import clsx from "clsx";
+
+import SearchForm from "components/Search/SearchForm";
+
+import { isDev } from "../../constants";
+import { prefixImgSrc, getGhlErrorStatus } from "helper";
+
+const useStyles = makeStyles((_theme) => ({
+  umamusumeListWrapper: {
+    maxWidth: "880px",
+    margin: "auto",
+  },
+  addButton: {
+    position: "fixed",
+    bottom: "1.5rem",
+    right: "1rem",
+  },
+  cardRoot: {
+    display: "flex",
+    justifyContent: "center",
+    width: "10rem",
+    height: "10rem",
+    marginRight: "16px",
+    marginBottom: "16px",
+  },
+  cardMedia: {
+    width: "6.6rem",
+    height: "6.6rem",
+    backgroundPosition: "top center",
+    margin: 0,
+  },
+  cardName: {
+    wordBreak: "keep-all",
+    textAlign: "center",
+  },
+  linkWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  listWrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+}));
+
+export const UmamusumeList = ({ data, statusCode }) => {
+  const classes = useStyles();
+  const [umamusumeList, setUmamusumeList] = useState(data.umamusumeList);
+
+  if (statusCode) {
+    return <Error statusCode={statusCode} />;
+  }
+
+  return (
+    <div className={classes.umamusumeListWrapper}>
+      <h1>우마무스메 리스트</h1>
+      <SearchForm
+        data={data.umamusumeList}
+        handleSearch={setUmamusumeList}
+        searchType={"Umamusume"}
+      />
+      <div className={classes.listWrapper}>
+        {umamusumeList.map(({ name, id, imageSrc }) => {
+          return (
+            <Card className={clsx(classes.cardRoot)} key={id + name.default}>
+              <Link href={`/umamusume/${id}`}>
+                <a className={clsx(classes.linkWrapper)}>
+                  <CardMedia
+                    className={clsx(classes.cardMedia)}
+                    image={prefixImgSrc(imageSrc || "/image/temp.png")}
+                    title={name.en}
+                  />
+                  <CardContent classes={{ root: classes.cardName }}>
+                    {name.ko}
+                  </CardContent>
+                </a>
+              </Link>
+            </Card>
+          );
+        })}
+      </div>
+      {isDev && (
+        <Link href={"/admin/umamusume/new"}>
+          <a className={classes.addButton}>
+            <AddCircleOutlineRoundedIcon fontSize="large" color="primary" />
+          </a>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default UmamusumeList;
