dd08ad569bdcc02cfc2ea1d81f2935839bcb7810
ExtraCold <40051225+extracold1209@users.noreply.github.com>
2022-04-19 21:31:30 +0900

    chore: 반복 컴포넌트 리팩토링 (#45)
    - 반복컴포넌트 보기 좋지 않은데스

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -24,46 +24,54 @@ const useStyles = makeStyles((_theme) => ({
   },
 }));
 
+const routes: {title: string; subroutes: {href: string, text: string}[]}[] = [{
+  title: "우마무스메 관리",
+  subroutes: [{
+    href: "/admin/umamusume",
+    text: "우마무스메 리스트"
+  }, {
+    href: "/admin/umamusume/new",
+    text: "우마무스메 추가"
+  }]
+}, {
+  title: "카드 관리",
+  subroutes: [{
+    href: "/cards",
+    text: "카드 리스트"
+  }, {
+    href: "/admin/cards/new",
+    text: "카드 추가"
+  }]
+}, {
+  title: "스킬 관리",
+  subroutes: [{
+    href: "/skills",
+    text: "스킬 리스트"
+  }, {
+    // NOTE 뭔가 이상함
+    href: "/cards",
+    text: "스킬 리스트"
+  }]
+}]
+
 export const Admin = () => {
   const classes = useStyles();
   return (
     <div className={clsx(classes.root)}>
       <List component="ul">
-        <ListSubheader>우마무스메 관리</ListSubheader>
-        <ListItem>
-          <Link href="/admin/umamusume">
-            <ListItemText primary="우마무스메 리스트" />
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="/admin/umamusume/new">
-            <ListItemText primary="우마무스메 추가" />
-          </Link>
-        </ListItem>
-        <Divider />
-        <ListSubheader>카드 관리</ListSubheader>
-        <ListItem>
-          <Link href="/cards">
-            <ListItemText primary="카드 리스트" />
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="/admin/cards/new">
-            <ListItemText primary="카드 추가" />
-          </Link>
-        </ListItem>
-        <Divider />
-        <ListSubheader>스킬 관리</ListSubheader>{" "}
-        <ListItem>
-          <Link href="/skills">
-            <ListItemText primary="스킬 리스트" />
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link href="/cards">
-            <ListItemText primary="스킬 리스트" />
-          </Link>
-        </ListItem>
+        {routes.map(({title, subroutes}, index) => (
+          <React.Fragment key={title}>
+            <ListSubheader>{title}</ListSubheader>
+            {subroutes.map(({href, text}) => (
+              <ListItem key={href}>
+                <Link href={href}>
+                  <ListItemText primary={text}/>
+                </Link>
+              </ListItem>
+            ))}
+            {index !== routes.length - 1 ? <Divider/> : <></>}
+          </React.Fragment>
+        ))}
       </List>
     </div>
   );
