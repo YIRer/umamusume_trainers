7efc2580ce24f32eed4ea24ee6fc3b1bf45048bd
YIRer <xeroonelove@gmail.com>
2022-06-21 22:18:23 +0900

    fix: SEO 버그 수정

--- src/components/Card/helper.ts
+++ src/components/Card/helper.ts
@@ -11,8 +11,9 @@ export const supportTypes = {
 };
 
 export const formattedDescriptionForCards = (cardData: CardType): string => {
+  
   const formattedStatus = cardData.playable
-    ? `더트 ${cardData.status.ground.duct}, 잔디 ${cardData.status.ground.turf}, 단거리 ${cardData.status.distance.short}, 마일 ${cardData.status.distance.mile}, 중거리 ${cardData.status.distance.short}, 단거리 ${cardData.status.distance.short}, 단거리 ${cardData.status.distance.medium}, 장거리 ${cardData.status.distance.long}, 도주 ${cardData.status.strategy.escape}, 선행 ${cardData.status.strategy.leading}, 선입 ${cardData.status.strategy.between}, 추입 ${cardData.status.strategy.pushing}, 스피드 ${cardData.status.status.speed}, 스태미나 ${cardData.status.status.stamina}, 파워 ${cardData.status.status.power}, 근성 ${cardData.status.status.guts}, 지능 ${cardData.status.status.intelligence}`
+    ? `더트 ${cardData.status.ground.duct.rank}, 잔디 ${cardData.status.ground.turf.rank}, 단거리 ${cardData.status.distance.short.rank}, 마일 ${cardData.status.distance.mile.rank}, 중거리 ${cardData.status.distance.medium.rank}, 장거리 ${cardData.status.distance.long.rank}, 도주 ${cardData.status.strategy.escape.rank}, 선행 ${cardData.status.strategy.leading.rank}, 선입 ${cardData.status.strategy.between.rank}, 추입 ${cardData.status.strategy.pushing.rank}, 스피드 ${cardData.status.status.speed.rank}, 스태미나 ${cardData.status.status.stamina.rank}, 파워 ${cardData.status.status.power.rank}, 근성 ${cardData.status.status.guts.rank}, 지능 ${cardData.status.status.intelligence.rank}`
     : `${supportTypes[cardData.supportType]}`;
 
   let star = "";
